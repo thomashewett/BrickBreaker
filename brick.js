9d45024ca9da5eb669886ab48010247576e0a746
thomashewett <thomashewett@hotmail.com>
2020-10-12 16:20:45 +1100

    Added Bricks

--- brick.js
+++ brick.js
@@ -1,15 +1,38 @@
+//canvas variables
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
+
+//ball variables
 var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+//paddle variables
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2
 var rightBtnPressed = false;
 var leftBtnPressed = false;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+
+//brick variables
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
 
 //Event listeners to watch for a keydown or keyup press
 document.addEventListener("keydown", keyDownHandler, false);
@@ -35,6 +58,20 @@ function keyUpHandler(e) {
     }
 }
 
+function collisionDetection(){
+    for(var c = 0; c<brickColumnCount; c++){
+        for(var r=0; r< brickRowCount; r++){
+            var b = bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function drawBall(){
 
     //start drawing a path
@@ -57,6 +94,24 @@ function drawPaddle(){
     ctx.closePath();
 }
 
+function drawBricks(){
+    for(var c=0; c<brickColumnCount; c++){
+        for(var r=0; r<brickRowCount; r++){
+            if(bricks[c][r].status == 1){
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
 /*
     re-occuring function that is called every 10ms
     draws the ball and the paddle then checks for 
@@ -64,8 +119,10 @@ function drawPaddle(){
 */
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     //check if ball has collided with side walls
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
